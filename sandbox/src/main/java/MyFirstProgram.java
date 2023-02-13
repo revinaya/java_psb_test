27c16e8aa22882e4f7f334ccafd334accce05fd0
revinaya <revinaya@gmail.com>
2023-02-13 22:42:40 +0300

    Д/з №2 п.1-3

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,16 @@
 public class MyFirstProgram {
-	
-	public static void main (String [] args) {
-		System.out.println("Hello, world");
-	}
-	
+
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(4, 4);
+        System.out.println("Расстояние между двумя точками = " + distance(p1, p2));
+
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
 }
