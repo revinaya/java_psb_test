d534432063304d1b5ed6c6aad00e60e7a2af9a2d
revinaya <revinaya@gmail.com>
2023-02-25 19:36:36 +0300

    Д/з № 7 - Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,21 +14,30 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private HelperBase helperBase;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-      wd = new FirefoxDriver();
-      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-      wd.get("http://localhost:8080/addressbook/");
-      groupHelper = new GroupHelper(wd);
-      contactHelper = new ContactHelper(wd);
-      navigationHelper = new NavigationHelper(wd);
-      sessionHelper = new SessionHelper(wd);
-      helperBase = new HelperBase(wd);
-      sessionHelper.login("admin", "secret");
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost:8080/addressbook/");
+        groupHelper = new GroupHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        helperBase = new HelperBase(wd);
+        sessionHelper.login("admin", "secret");
     }
 
     public void stop() {
-      wd.quit();
+        wd.quit();
     }
 
     public GroupHelper getGroupHelper() {
