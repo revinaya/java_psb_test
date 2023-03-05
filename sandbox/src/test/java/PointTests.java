eddae14f4c63765901340905f1209e996ec10541
revinaya <revinaya@gmail.com>
2023-03-05 20:38:15 +0300

    Д/з №2 и 3  - исправления

--- sandbox/src/test/java/PointTests.java
+++ sandbox/src/test/java/PointTests.java
@@ -8,19 +8,27 @@ public class PointTests {
     public void testDistance1() {
         Point p1 = new Point(10, 10);
         Point p2 = new Point(10, 5);
-        Assert.assertEquals(Point.distance(p1, p2), 5.0);
+        Assert.assertEquals(p1.distance(p2), 5.0);
     }
     @Test
     public void testDistance2() {
         Point p1 = new Point(-8, 0);
         Point p2 = new Point(4, -2);
-        Assert.assertEquals(Point.distance(p1, p2), 12.165525060596439);
+        Assert.assertEquals(p1.distance(p2), 12.165525060596439);
     }
+
 
     @Test
     public void testDistance3() {
         Point p1 = new Point(7, 0);
         Point p2 = new Point(0, 0);
-        Assert.assertEquals(Point.distance(p1, p2), 7.0);
+        Assert.assertEquals(p1.distance(p2), 7.0);
+    }
+
+    @Test
+    public void testDistance4() {
+        Point p1 = new Point(7, 0);
+        Point p2 = new Point(0, 0);
+        Assert.assertEquals(MyFirstProgram.distance(p1, p2), 7.0);
     }
 }
