eb30051e2f6594ade5047523c4aae2d283796b4c
revinaya <revinaya@gmail.com>
2023-04-15 20:16:15 +0300

    Для д/з 21

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -24,7 +24,7 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
     public static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
